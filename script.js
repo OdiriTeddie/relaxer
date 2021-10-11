1c9b635aa740709516c1ff935d5b18699b3445f1
Odiri Teddie <odiriteddie.adamu@gmail.com>
2021-10-10 21:14:30 -0400

    audio not playing on mobile device fix

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+body = document.querySelector('body');
+audio = document.querySelector('audio');
 
+console.log(audio);
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
@@ -23,3 +26,9 @@ function breathAnimation() {
 }
 
 setInterval(breathAnimation, totalTime);
+
+function playAudio() {
+    audio.play();
+}
+
+body.addEventListener('click', playAudio);
